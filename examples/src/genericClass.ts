985730cf2e59a49678b4fe781c54435b7368b54f
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-06-17 16:04:21 +0700

    Add generic class example

---
+++ examples/src/genericClass.ts
@@ -0,0 +1,7 @@
+class GenericNumber<NumType> {
+    zeroValue!: NumType;
+    add!: (x: NumType, y: NumType) => NumType;
+}
+const myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = (x, y) => x + y;
