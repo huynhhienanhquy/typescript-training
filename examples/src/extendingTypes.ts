e208a76e7ed038aa4c71d3aab562900a23a4227f
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-06-17 16:04:21 +0700

    Add extending types example

---
+++ examples/src/extendingTypes.ts
@@ -0,0 +1,12 @@
+interface Person {
+    name: string;
+}
+
+interface Student extends Person {
+    studentId: string;
+}
+
+const student: Student = {
+    name: "Quy",
+    studentId: "S12345"
+};
