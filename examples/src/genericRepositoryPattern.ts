22843546c8c38d8e27d2df60ca690dc3fefb6c2c
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-06-17 16:04:21 +0700

    Add generic repository pattern example

---
+++ examples/src/genericRepositoryPattern.ts
@@ -0,0 +1,17 @@
+// Common interface for database data manipulation classes
+interface IRepository<T> {
+    getAll(): T[];
+    getById(id: number): T | undefined;
+    create(item: T): void;
+}
+
+// Áp dụng cụ thể cho đối tượng User
+interface User2 { id: number; username: string; }
+
+class UserRepository implements IRepository<User> {
+    private users: User[] = [];
+
+    getAll(): User[] { return this.users; }
+    getById(id: number): User | undefined { return this.users.find(u => u.id === id); }
+    create(user: User): void { this.users.push(user); }
+}
