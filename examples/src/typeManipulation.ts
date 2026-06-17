6c5a465002a120154b6f03044b010c3fa366ab23
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-06-17 16:04:22 +0700

    Add type manipulation example

---
+++ examples/src/typeManipulation.ts
@@ -0,0 +1,24 @@
+const user = {
+  id: 1,
+  name: "Quy",
+  age: 22,
+};
+
+// typeof
+type User = typeof user;
+
+// keyof
+type UserKey = keyof User;
+
+// Generic
+function updateUser<K extends UserKey>(
+  key: K,
+  value: User[K]
+) {
+  user[key] = value;
+}
+
+updateUser("name", "Quy Huynh");
+updateUser("age", 25);
+
+console.log(user);
