aaae37e566506c9fa99e0e4e9cf3fd540510ddc6
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-06-17 16:04:21 +0700

    Add generic example

---
+++ examples/src/generic.ts
@@ -0,0 +1,25 @@
+interface User1 {
+  id: number;
+  name: string;
+}
+
+interface ApiResponse<T> {
+  success: boolean;
+  data: T;
+}
+
+function getData<T>(
+  data: T
+): ApiResponse<T> {
+  return {
+    success: true,
+    data,
+  };
+}
+
+const response = getData<User1>({
+  id: 1,
+  name: "Quy",
+});
+
+console.log(response);
