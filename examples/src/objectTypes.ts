51d692d3ab58031baf037ec86d1552a468910f73
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-06-17 16:04:22 +0700

    Add object types example

---
+++ examples/src/objectTypes.ts
@@ -0,0 +1,23 @@
+// 1. Readonly
+interface Book {
+    readonly title: string;
+}
+const myBook: Book = { title: "Học TypeScript" };
+
+// 2. Index Signature (When you don't know what the key consists of beforehand.)
+interface StringArray {
+    [index: number]: string; // Key is a number, Value is a letter.
+}
+const myArray: StringArray = ["Cường", "Quy"];
+
+// 3. Extending Types (extends) and Intersection Types (&)
+interface Colorful { color: string; }
+interface CircleInterface { radius: number; }
+
+// Extends via Interface
+interface ColorfulCircle extends Colorful, CircleInterface {}
+
+// Representation using Intersection (Type)
+type ColorfulCircleType = Colorful & CircleInterface;
+
+const cc: ColorfulCircleType = { color: "red", radius: 42 };
