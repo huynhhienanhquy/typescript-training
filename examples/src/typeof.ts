dcf53416b072c5e5b0428bc7aa8d2debb57b24fb
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-06-18 16:22:00 +0700

    Add typeof example

---
+++ examples/src/typeof.ts
@@ -0,0 +1,27 @@
+const user4 = {
+    name: "Quy",
+    age: 22
+};
+
+type userType = typeof user4;
+// type UserType = {
+//     name: string;
+//     age: number;
+// }
+
+//Reuse object types
+const laptop = {
+    brand: "Dell",
+    model: "XPS 13",
+    price: 999
+};
+
+type laptopType = typeof laptop;
+
+const anotherLaptop: laptopType = {
+    brand: "Apple",
+    model: "MacBook Air",
+    price: 1199
+};
+
+console.log(anotherLaptop);
