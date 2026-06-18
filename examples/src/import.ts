dfcc1da24523d443962370711464cc14c48c4848
huynhhienanhquy2005 <quyhuynh.tp2005@email.com>
2026-06-18 16:18:10 +0700

    Add Import example

---
+++ examples/src/import.ts
@@ -0,0 +1,11 @@
+import { PI, add } from "./export";
+
+import User from "./export";
+
+import * as MathUtils from "./export";
+
+MathUtils.PI;
+MathUtils.add(1, 2);
+
+import type { user1 } from "./export";
+
